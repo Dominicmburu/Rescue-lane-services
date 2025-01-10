4ee572b61a189111fc88c2120421e97692aa2dea
Potmore <mbugwapatrik@gmail.com>
2025-01-10 23:03:39 +0300

    Homepage styling

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, createBrowserRouter, RouterProvider } from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
-import Services from './Pages/Services/services';
-import Contact from './Pages/ContactForm/ContactForm';
+import Services from './Pages/Services/Services';
+import Contact from './Pages/Contact/Contact';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/services",
-    element: <Services />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  },
-]);
-
-function App() {
+const App: React.FC = () => {
   return (
-    <RouterProvider router={router} /> 
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
